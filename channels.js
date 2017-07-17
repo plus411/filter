3e15af711fe4576425ed4a6040f2a204789b9996
plus411 <Plus41101@yahoo.com>
2017-07-17 19:13:40 -0400

    Channel test

--- channels.js
+++ channels.js
@@ -19,7 +19,7 @@ module.exports = {
             break;
                 
             case 'remove':
-                console.log(message.guild.channels)
+                console.log(message.guild.channels.findAll('name', 'auto'))
                 
             break;
                 

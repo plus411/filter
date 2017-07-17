52c21eb91c4f4455984acda54d0801a24ea8b97a
plus411 <Plus41101@yahoo.com>
2017-07-16 22:03:03 -0400

    Fixed unwanted recursion
    Made it so that filter will not respond to it's own message when adding messages to the admin log.

--- autoadmin.js
+++ autoadmin.js
@@ -3,6 +3,8 @@
 
 module.exports = {
     log: function(message) {
+        if (message.channel != 'adminlog') {
         message.guild.channels.find('name', 'adminlog').sendMessage(message.channel + ' : ' + message.author + ' : ' + message.content);
+        }
     }
 }

3c48f828b43e71e8dec71dca704b5885af105b43
plus411 <Plus41101@yahoo.com>
2017-07-31 01:31:26 -0400

    Rebuilt schedule
    Rebuilt the schedule module to use moment.js for the date structure

--- schedule.js
+++ schedule.js
@@ -1,56 +1,31 @@
 // Schedule.js
 // Allows mods and admins to create a schedule and players to view it
+
+const moment = require('moment.js'); 
+
 var schedule = [
-  { date: new Date(2017, 7, 4, 16),
+  { date: moment('2017/08/05-04:30pm', 'YYYY/MM/DD-hh:mmaa'),
     title: 'karaoke night',
     description: "Let's sing! Serenade us with the song of your peoples at our biweekly karaoke night!" } 
+
+  { date: moment('2017/08/11-04:00pm', 'YYYY/MM/DD-hh:mmaa'),
+    title: 'minigame night',
+    description: "The Aquarium's Biweekly minigame night. Go head to head against other fish in a competitive minigame!" }
 ]; 
 
 module.exports = {
-  groom: function() {
-    var now = new Date();
-    
-    schedule.sort(function (a, b) { return a.date.getTime() - b.date.getTime() });
-    
-    schedule.forEach(function (object, index) {
-      if ( object.date.getTime() < now.getTime() ) {
-        schedule.splice(index, 1);
-      }
-    });
-  },
-  
   build: function() {
-    var msgArray = ['***Upcoming Events:***'];
-    
-    schedule.forEach(function (object) {
-      var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-      var dayNames = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-      
-      var weekday = dayNames[object.date.getDay()];
-      var month = monthNames[object.date.getMonth()];
-      var day = object.date.getDate();
-      var hour = object.date.getHours();
-      
-      if (hour <= 12) { hour = hour + "AM ET"; } 
-      else { 
-        hour = hour - 12;
-        hour = hour + "PM ET";
-      }
-      
-      msgArray.push('**' + object.title.toUpperCase() + '**');
-      msgArray.push('*' + weekday + ', ' + month + ' ' + day + ' at ' + hour + '*');
-      msgArray.push(object.description);
-      msgArray.push('---');
+    var msgArray = ['***UPCOMING EVENTS:***'];
+
+    schedule.forEach(function(object) {
+      msgArray.push('**' + object.title + '**');
+      msgArray.push('*' + object.date.format('dddd, MMMM D [at] h:m A') + '*');
+      msgArray.push('*in ' + object.date.fromNow() + '*');
+      msgArray.push(schedule.description);
     });
-    
-    msgArray.pop();
-    
-    return msgArray;
-    
-  },
-  
+  }
+
   display: function(message) {
-    module.exports.groom();
-    message.channel.send(module.exports.build());
+
   }
 }

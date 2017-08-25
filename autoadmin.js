b937ac8cf9043d15b22548cf20ae0ba07720cc5c
plus411 <Plus41101@yahoo.com>
2017-08-25 04:12:41 -0400

    Filter rules crash hotfix.

--- autoadmin.js
+++ autoadmin.js
@@ -3,10 +3,12 @@
 
 module.exports = {
     log: function(message) {
-        if (message.channel != message.guild.channels.find('name', 'adminlog')) {
-            message.guild.channels.find('name', 'adminlog').sendMessage(message.channel + ' : ' + message.author + ' : ' + message.content);
-            
-            if (message.attachments.first()) { message.guild.channels.find('name', 'adminlog').send(message.attachments.first().url); }
+        if (message.guild.channels.find('name', 'adminlog')) {
+            if (message.channel != message.guild.channels.find('name', 'adminlog')) {
+                message.guild.channels.find('name', 'adminlog').sendMessage(message.channel + ' : ' + message.author + ' : ' + message.content);
+                
+                if (message.attachments.first()) { message.guild.channels.find('name', 'adminlog').send(message.attachments.first().url); }
+            }
         }
     }
 }

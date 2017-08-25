9732540001ffa6557026414d564ffdadd811f47e
plus411 <Plus41101@yahoo.com>
2017-08-25 04:16:57 -0400

    hotfix

--- autoadmin.js
+++ autoadmin.js
@@ -3,7 +3,7 @@
 
 module.exports = {
     log: function(message) {
-        if (message.guild.channels.find('name', 'adminlog')) {
+        if (message.guild) {
             if (message.channel != message.guild.channels.find('name', 'adminlog')) {
                 message.guild.channels.find('name', 'adminlog').sendMessage(message.channel + ' : ' + message.author + ' : ' + message.content);
                 

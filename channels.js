e775908f5e054c941b8a90ff973a36defbbe33a3
plus411 <Plus41101@yahoo.com>
2017-09-02 00:43:30 -0400

    Added autoPurge of voice channels

--- channels.js
+++ channels.js
@@ -2,25 +2,28 @@
 // Used to dynamically create and destroy voice channels when nessesary
 
 module.exports = {
-    autoPurge: function() {
-
-    },
-
     remove: function(message, command) {
         message.guild.channels.findAll('name', 'auto').forEach(function(channel, item) {
-            console.log(item);
             if (channel.members.array().length == 0) {
-                //console.log(channel.members.array().length)
                 channel.delete()
             }
         })
+    },
+
+    autoPurge: function(message, command) {
+        if (message.guild.channels.findAll('name', 'auto').length > 0) {
+            module.exports.remove(message, command);
+            setTimeout(() => { module.exports.autoPurge(message, command) }, 30000);
+        }
     },
 
     channelmod: function(message,command) {
         switch (command[2]) {
         
             case 'add':
-                if (command[3] > 0) { var amount = command[3] }
+                if (command[3] > 0) { 
+                    var amount = command[3] 
+                }
                 else { 
                     message.channel.send('Try formatting the command like this: _filter voice add <amount>_') 
                     return;
@@ -29,6 +32,8 @@ module.exports = {
                 for (var i = 0; i < amount; i++) {
                     message.guild.createChannel('auto', 'voice')
                 }
+
+                module.exports.autoPurge(message, command);
                 
             break;
                 

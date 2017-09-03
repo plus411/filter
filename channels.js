dc9be2ec95a9f9fc4cc8f89f4a66d41c5b62009d
plus411 <Plus41101@yahoo.com>
2017-09-03 03:39:15 -0400

    Timgings

--- channels.js
+++ channels.js
@@ -60,7 +60,7 @@ module.exports = {
                                 moveable[0].setPosition(-3, true);
                                 moveable.shift();
                                 if (moveable.length > 0) { moveChannel(); }
-                            }, 1000);
+                            }, 5000);
                         }
 
                         console.log(defaultChannels.length);
